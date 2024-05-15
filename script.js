55faed4d7803c6d07b758814095cb8fb87002ea3
GustavoORl <124843237+GustavoORl@users.noreply.github.com>
2024-05-15 12:22:52 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,16 @@
-var video = document.getElementById('video');
-var playBtn = document.getElementById('video-play');
-var pauseBtn = document.getElementById('video-pause');
+const box = document.querySelector(".container");
+const imagens = document.querySelectorAll(".container .img");
 
+let contador = 0;
+
+function slider(){
+    contador ++
+
+    if (contador > imagens.length - 1){
+        contador = 0;
+    }
+
+    box.style.transform = `translateX(${-contador * 400}px)`;
+}
+
+setInterval(slider, 5000);
